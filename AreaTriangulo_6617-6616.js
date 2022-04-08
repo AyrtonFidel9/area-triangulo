f3d79cd82924db50002b5ef0bf8345728d06f9a0
Paul Sebastián Bolaños Hidalgo <75400264+SBola97@users.noreply.github.com>
2022-04-08 08:39:50 -0500

    Update AreaTriangulo_6617-6616.js

--- AreaTriangulo_6617-6616.js
+++ AreaTriangulo_6617-6616.js
@@ -3,17 +3,27 @@ let base;
 let altura;
 let tipot;
 let medida;
-
+/*
 function getData() {
   tipot = document.getElementById("tipost").value;
   base = parseInt(document.getElementById("base").value);
   altura = parseInt(document.getElementById("altura").value);
   medida = document.getElementById("medidas").value
 }
+*/
+function getValue(input){
+  return parseInt(document.getElementById(input).value)
+}
+
+function validate(value){
+  return Number.isNaN(value);
+}
 
 function validar() {
-  getData();
-  if (!Number.isNaN(base) && !Number.isNaN(altura)) {
+  tipot = document.getElementById("tipost").value;
+  medida = document.getElementById("medidas").value;
+  hallar();
+  /*if (!Number.isNaN(base) && !Number.isNaN(altura)) {
     if(base !== 0 && altura !== 0 && base>0 && altura>0)
     {
       alert("Datos ingresados con éxito");
@@ -26,12 +36,26 @@ function validar() {
   }else{
     alert("No se admiten caracteres ni espacios vacíos");
     limpiar();
-  }
+  }*/
 }
 
 
 function hallar() {
   switch (tipot) {
+    case "Rectangulo":
+      if(getValue('ladoa') && getValue('ladob'))
+      {
+        if(!validate(getValue('ladoa')) && !validate(getValue('ladob'))){
+          document.getElementById("resultado").value = `${areaTR(getValue('ladoa'), getValue('ladob'))} ${medida}^2`;
+        }
+        else{
+          alert("error");
+        }
+      }
+      else{
+        alert("ingresar solo los datos para el lado a y b");
+      }
+      break;
     case "Isosceles":
       //alert(`El área es: ${areaTR(base, altura)} metros al cuadrado`);
       document.getElementById("resultado").value = `${areaTR(base, altura)} ${medida}^2`;
