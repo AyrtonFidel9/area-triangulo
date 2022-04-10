a18765a1c1b10f32aa4ab89e26816712b10fda08
AyrtonFidel9 <ayrtonfidel9@gmail.com>
2022-04-10 13:04:03 -0500

    Aregadas las validaciones de cada triangulo

--- AreaTriangulo_6617-6616.js
+++ AreaTriangulo_6617-6616.js
@@ -1,28 +1,105 @@
 const areaTR = (base, altura) => (base * altura) / 2;
-let base;
-let altura;
-let tipot;
-let medida;
-/*
+let datos = {};
+const resulatdo = document.querySelector("#resultado");
+const btnCalcular = document.querySelector("#calcular");
+resultado.disabled = true;
+resultado.style = "background-color: white";
+
+
 function getData() {
-  tipot = document.getElementById("tipost").value;
-  base = parseInt(document.getElementById("base").value);
-  altura = parseInt(document.getElementById("altura").value);
-  medida = document.getElementById("medidas").value
+  datos["base"]=parseFloat(document.getElementById("base").value);
+  datos["altura"]=parseFloat(document.getElementById("altura").value);
+  datos["ladoA"]=parseFloat(document.querySelector('#ladoa').value);
+  datos["ladoB"]=parseFloat(document.querySelector('#ladob').value);
+  datos["ladoC"]=parseFloat(document.querySelector('#ladoc').value);
+  datos["angA"]=parseFloat(document.querySelector('#anguloa').value);
+  datos["angB"]=parseFloat(document.querySelector('#angulob').value);
+  datos["angC"]=parseFloat(document.querySelector('#anguloc').value);
+  datos["medida"]=document.querySelector("#medidas").value;
 }
-*/
+
 function getValue(input){
   return parseInt(document.getElementById(input).value)
 }
 
-function validate(value){
-  return Number.isNaN(value);
+function countNotNaN(){
+  let count = 0;
+  Object.entries(datos).forEach(([key, value])=>{
+      count = (!validate(value)) ? count + 1 : count;
+  });
+  return count;
 }
 
-function validar() {
-  tipot = document.getElementById("tipost").value;
-  medida = document.getElementById("medidas").value;
-  hallar();
+function countNegativesAndZeros(){
+  let count = 0;
+  Object.entries(datos).forEach(([key, value])=>{
+      count = (value <= 0) ? count + 1 : count;
+  });
+  return count;
+}
+
+function validate(value){
+  return Number.isNaN(value); // si el valor no es un numero o esta vacio retorna verdadero
+}
+
+
+function foundTriangle(){
+  // comprueba si los valores no son vacios o cadenas
+  /**+++
+   * donde esta el return "Escaleno", "Isoceles", etc hay q devolver un objeto asi
+   * {resultado: (con la funcion q calcule los datos), tipo: (con el tipo de triangulo q es), medida:(la medida utilizada)}
+   */
+  if(!validate(datos.ladoA) && !validate(datos.ladoB)){
+    return (datos.ladoA !== datos.ladoB) ? "Rectangulo" : "Isoceles";
+  }
+  else if(!validate(datos.ladoA) || (!validate(datos.ladoA)&&!validate(datos.ladoB)&&!validate(datos.ladoC))){
+    return (datos.ladoA === datos.ladoB && datos.ladoB === datos.ladoC && datos.ladoC == datos.ladoA)
+      ? "Equilátero" : "Escaleno";
+  }
+  else if(!validate(datos.ladoA) && !validate(datos.ladoC) && !validate(datos.angC)){
+    return "Isóceles";
+  }
+  else if (!validate(datos.base) && !validate(datos.altura)){
+    return "Escaleno"; // o tirangulo cualquiera
+  }
+  else if (!validate(datos.ladoA) && !validate(datos.angB) && !validate(datos.angC)){
+    return "Escaleno"; // o tirangulo cualquiera 
+  }
+  else if (!validate(datos.ladoB) && !validate(datos.angA) && !validate(datos.angC)){
+    return "Escaleno"; // o tirangulo cualquiera 
+  }
+  else if (!validate(datos.ladoC) && !validate(datos.angA) && !validate(datos.angB)){
+    return "Escaleno"; // o tirangulo cualquiera 
+  } 
+  else if (!validate(datos.ladoA) && !validate(datos.ladoB) && !validate(datos.angC)){
+    return "Escaleno"; // o tirangulo cualquiera 
+  }
+  else if (!validate(datos.ladoA) && !validate(datos.ladoC) && !validate(datos.angB)){
+    return "Escaleno"; // o tirangulo cualquiera 
+  }
+  else if (!validate(datos.ladoB) && !validate(datos.ladoC) && !validate(datos.angA)){
+    return "Escaleno"; // o tirangulo cualquiera 
+  } 
+}
+
+function calcular() {
+  getData();
+  if(countNotNaN() - 1 === 0){
+    alert("No se admiten campos vacios");
+  } else if (countNegativesAndZeros() > 0){
+    alert("No se admiten número negativos ni tampoco Ceros");
+  }
+  else if(countNotNaN() - 1 > 3){
+    alert("Error, exceso de datos enviados para calcular el area de un triángulo");
+  }
+  else{
+    //hallar();
+    console.log(foundTriangle());
+    console.log(datos);
+
+  }
+  
+  //hallar();
   /*if (!Number.isNaN(base) && !Number.isNaN(altura)) {
     if(base !== 0 && altura !== 0 && base>0 && altura>0)
     {
@@ -39,7 +116,7 @@ function validar() {
   }*/
 }
 
-
+/*
 function hallar() {
   switch (tipot) {
     case "Rectangulo":
@@ -75,9 +152,12 @@ function hallar() {
       alert("No existe cálculos para esa opción");
   }
 }
-
+*/
 function limpiar() {
   document.getElementById("base").value = "";
   document.getElementById("altura").value = "";
   document.getElementById("resultado").value = "";
 }
+
+
+btnCalcular.addEventListener('click',calcular);
